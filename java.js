0f3b8a501dc6637fec54a21f16ab02910d4fcb61
vicky_08 <vivekhiremath1440@gmail.com>
2019-08-20 19:50:59 +0530

    Initial push

---
+++ java.js
@@ -0,0 +1,15 @@
+//Dropdown Hover
+    $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+ 
